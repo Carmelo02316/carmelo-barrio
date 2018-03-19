5b5828a9c59bd4ac7684513be7e50863fc9b1fa1
Carmelo Barrio nuevo rosales <carmelo.barrionuevorosales@stu.jefferson.kyschools.us>
2018-03-19 09:41:09 -0400

    adding dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+sports_def = {
+    "boxing": "the sport or practice of fighting with the fists, especially with padded gloves in a roped square ring according to prescribed rules",
+    "soccer": "a game played by two teams of eleven players with a round ball that may not be touched with the hands or arms during play except by the goalkeepers. The object of the game is to score goals by kicking or heading the ball into the opponents' goal",
+    "basketball": "a game played between two teams of five players in which goals are scored by throwing a ball through a netted hoop fixed above each end of the court.",
+    "baseball": "a ball game played between two teams of nine on a field with a diamond-shaped circuit of four bases. It is played chiefly in the US, Canada, Latin America, and East Asia",
+    "ice hockey": "a fast contact sport played on an ice rink between two teams of six skaters, who attempt to drive a small rubber disk (the puck) into the opposing goal with hooked or angled sticks. It developed in Canada in the 19th century",
+    "football": ".a form of team game played in North America with an oval ball on a field marked out as a gridiron.",
+    }
+print sports_def
